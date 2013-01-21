acb9a275f051b88aa32c07aa74c306752c8a0697
Serge S. Koval <serge.koval+github@gmail.com>
2013-01-21 12:36:41 +0200

    More examples

---
+++ examples/auth.py
@@ -0,0 +1,24 @@
+import logging
+import tornado.ioloop
+from toredis import Client
+
+
+def test_auth():
+    # Assuming that 12345 is your redis pasword
+    redis.auth('12345', after_auth)
+
+
+def after_auth(status):
+    print 'Authentication status:', status
+    assert status == 'OK'
+    io_loop.stop()
+
+
+if __name__ == "__main__":
+    logging.basicConfig()
+
+    io_loop = tornado.ioloop.IOLoop.instance()
+
+    redis = Client()
+    redis.connect('localhost', callback=test_auth)
+    io_loop.start()
---
+++ examples/genauth.py
@@ -0,0 +1,28 @@
+import logging
+import tornado.ioloop
+from tornado import gen
+from toredis import Client
+
+
+@gen.engine
+def test():
+    # Authenticate first
+    status = yield gen.Task(redis.auth, '12345')
+    assert status == 'OK'
+
+    # Select database
+    status = yield gen.Task(redis.select, '0')
+    assert status == 'OK'
+
+    print 'Success'
+
+    io_loop.stop()
+
+if __name__ == "__main__":
+    logging.basicConfig()
+
+    io_loop = tornado.ioloop.IOLoop.instance()
+
+    redis = Client()
+    redis.connect('localhost', callback=test)
+    io_loop.start()
