e9d949a9a26e6f3ef44f8feb3a516574b4247996
Ovidiu Predescu <ovidiu@gmail.com>
2013-04-27 00:12:22 -0700

    Version bump.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ except ImportError:
 
 setup(
     name='toredis',
-    version='0.1.1',
+    version='0.1.2',
     description='Really simple async Redis client for Tornado',
     author='Josh Marshall',
     author_email='catchjosh@gmail.com',
